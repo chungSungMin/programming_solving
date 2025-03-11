db21551a02aac0f941a817f7d816ded8a5965cf4
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2025-03-11 13:14:09 +0900

    [Silver II] Title: 로또, Time: 136 ms, Memory: 111996 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,27 @@
+
+def dfs(result, depth, now):
+    if depth == 6 :
+        print(*result)
+        return
+
+    else :
+        for i in range(now, K):
+            if depth + K - i < 6 :
+                return
+            else :
+                result.append(S[i])
+                dfs(result, depth + 1, i + 1)
+                result.pop()
+
+
+if __name__ == '__main__':
+    while True :
+        test = list(map(int, input().split()))
+        result = []
+        if test[0] == 0 :
+            break
+        else :
+            K = test[0]
+            S = test[1 : ]
+            dfs(result, 0,0)
+            print()
